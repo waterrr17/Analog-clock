90f85ac5945daeded79f9e111a7b8bf32437e060
waterrr17 <mj0904meeju@naver.com>
2021-12-06 17:15:24 +0900

    Add tooltip

--- src/Clock.js
+++ src/Clock.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './clock.css';
+import ReactTooltip from 'react-tooltip';
 
 class Clock extends Component {
     componentDidMount(){
@@ -7,20 +8,19 @@ class Clock extends Component {
         setInterval(setTime, 1000);
     }
 
-    rotateDegree(time, unit){
-        switch(unit){
-            case 'h':
-                return ((time/24)*360)+90;
-            default:
-                return ((time/60)*360)+90;
-        }
-
+    rotateDegreeH(hour,min){
+        return (((hour+min*0.01)/12)*360)+90
+    }
+    rotateDegree(time){
+        return ((time/60)*360)+90;
     }
 
     render(){
         const {hour, min, sec} = this.props;
 
         return(
+        <div>
+            <a data-tip data-for="timeTooltip">
             <div class="clock">
                 <div class="clock-back">
                         <div class="marking marking-one"></div>
@@ -28,11 +28,16 @@ class Clock extends Component {
                         <div class="marking marking-three"></div>
                         <div class="marking marking-four"></div>
                         <div class="inner-back"></div>
-                    <div class="hand hour" style={{transform:`rotate(${this.rotateDegree(hour)}deg)`}}></div>
+                    <div class="hand hour" style={{transform:`rotate(${this.rotateDegreeH(hour,min)}deg)`}}></div>
                     <div class="hand min" style={{transform:`rotate(${this.rotateDegree(min)}deg)`}}></div>
                     <div class="hand sec" style={{transform:`rotate(${this.rotateDegree(sec)}deg)`}}></div>
                 </div>
-            </div>
+                </div>
+                </a>
+                <ReactTooltip id="timeTooltip" place="top" offset ="{'right': 20}" type="dark">
+                  <span>{hour<10? "0"+hour: hour}시 {min<10? "0"+min : min}분 {sec<10? "0"+sec:sec}초</span>
+                </ReactTooltip>
+        </div>
         )
     }
 }
