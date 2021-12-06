e6096cedfbd4d6a1150b78103c4b8b50e5b64cb7
waterrr17 <mj0904meeju@naver.com>
2021-12-06 17:35:41 +0900

    Error Fixed

--- src/Clock.js
+++ src/Clock.js
@@ -20,23 +20,24 @@ class Clock extends Component {
 
         return(
         <div>
-            <a data-tip data-for="timeTooltip">
             <div class="clock">
-                <div class="clock-back">
+                <a data-tip data-for="timeTooltip">
+                    <div class="clock-back">
                         <div class="marking marking-one"></div>
                         <div class="marking marking-two"></div>
                         <div class="marking marking-three"></div>
                         <div class="marking marking-four"></div>
                         <div class="inner-back"></div>
-                    <div class="hand hour" style={{transform:`rotate(${this.rotateDegreeH(hour,min)}deg)`}}></div>
-                    <div class="hand min" style={{transform:`rotate(${this.rotateDegree(min)}deg)`}}></div>
-                    <div class="hand sec" style={{transform:`rotate(${this.rotateDegree(sec)}deg)`}}></div>
-                </div>
-                </div>
+                        <div class="hand hour" style={{transform:`rotate(${this.rotateDegreeH(hour,min)}deg)`}}></div>
+                        <div class="hand min" style={{transform:`rotate(${this.rotateDegree(min)}deg)`}}></div>
+                        <div class="hand sec" style={{transform:`rotate(${this.rotateDegree(sec)}deg)`}}></div>
+                    </div>
                 </a>
-                <ReactTooltip id="timeTooltip" place="top" offset ="{'right': 20}" type="dark">
-                  <span>{hour<10? "0"+hour: hour}시 {min<10? "0"+min : min}분 {sec<10? "0"+sec:sec}초</span>
-                </ReactTooltip>
+            </div>
+            
+            <ReactTooltip id="timeTooltip" place="top" offset ="{'right': 20}" type="dark">
+                <span>{hour<10? "0"+hour: hour}시 {min<10? "0"+min : min}분 {sec<10? "0"+sec:sec}초</span>
+            </ReactTooltip>
         </div>
         )
     }
